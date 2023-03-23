5e4ff2dd02f7351d00c4eac7eb03f119624515c6
AXen-02 <50175472+AXen-02@users.noreply.github.com>
2023-03-24 06:43:51 +0800

    first commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2697368d4a642211"; }
+/******/ 		__webpack_require__.h = function() { return "eee9a844b44b9013"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
